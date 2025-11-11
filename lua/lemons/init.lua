5123cebe0ffb3c033fd66ff5d2e6ef9f6206ba28
Rh4096 <rhelvetican@gmail.com>
2025-11-11 16:04:26 +0700

    Don't clear the highlights and syntaxes

--- lua/lemons/init.lua
+++ lua/lemons/init.lua
@@ -1,9 +1,6 @@
 local M = {}
 
 function M.load()
-    vim.cmd.highlight("clear")
-    vim.cmd.syntax("reset")
-
     vim.o.termguicolors = true
     vim.o.background = "dark"
     vim.g.colors_name = "lemons"
