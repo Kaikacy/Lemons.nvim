b0cd35532d02f21d8dd584bad3d70b39f6ca319a
Kaikacy <lukakuka735@gmail.com>
2026-01-12 18:47:44 +0400

    update highlights

--- lua/lemons/colors.lua
+++ lua/lemons/colors.lua
@@ -1,23 +1,22 @@
 ---@class lemons.Colors
----@field black string              |"#000000"
----@field dark_gray string          |"#161616"
----@field gray string               |"#212121"
----@field light_gray string         |"#565656"
----@field darker_white string       |"#808080"
----@field dark_white string         |"#cacaca"
----@field white string              |"#f0f0f0"
----@field red string                |"#ed505e"
----@field dark_green string         |"#0b1b10"
----@field lime string               |"#2ed592"
----@field green string              |"#2ed563"
----@field dark_yellow string        |"#1d190d"
----@field orange string             |"#fa8a49"
----@field yellow string             |"#f0be42"
----@field blue string               |"#5088ed"
----@field pink string               |"#f45ab4"
----@field dark_cyan string          |"#0c1918"
----@field cyan string               |"#37c3b5"
----@field light_cyan string         |"#6ad8ed"
+---@field black        string|"#000000"
+---@field dark_gray    string|"#161616"
+---@field gray         string|"#212121"
+---@field light_gray   string|"#565656"
+---@field darker_white string|"#808080"
+---@field dark_white   string|"#cacaca"
+---@field white        string|"#f0f0f0"
+---@field red          string|"#ed505e"
+---@field dark_green   string|"#0b1b10"
+---@field lime         string|"#2ed592"
+---@field green        string|"#2ed563"
+---@field orange       string|"#fa8a49"
+---@field yellow       string|"#f0be42"
+---@field blue         string|"#5088ed"
+---@field pink         string|"#f45ab4"
+---@field dark_cyan    string|"#0c1918"
+---@field cyan         string|"#37c3b5"
+---@field light_cyan   string|"#6ad8ed"
 
 local M = {}
 
@@ -29,13 +28,12 @@ M.defaults = {
     gray         = "#212121",
     light_gray   = "#565656",
     darker_white = "#808080",
-    dark_white   = "#cacaca",
-    white        = "#f0f0f0",
+    dark_white   = "#c0c0c0",
+    white        = "#e8e8e8",
     red          = "#ed505e",
     dark_green   = "#0b1b10",
     lime         = "#2ed592",
     green        = "#2ed563",
-    dark_yellow  = "#1d190d",
     orange       = "#fa8a49",
     yellow       = "#f0be42",
     blue         = "#5088ed",
